bf6245fb164a47adc12680a988c8be9ef2b11aca
Emeka <enwadibia@gmail.com>
2013-11-02 19:24:19 -0700

    entusic 1st push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Entusic::Application.config.secret_key_base = 'a3a1169052822c699c7bdaf823ebbb8f174d5699902220c3ebdb47f6237be8622f65a74bde2465f29fe06173b452d1cf2bb3a74d30ad5a7e2393efb57e86af37'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Entusic::Application.config.session_store :cookie_store, key: '_Entusic_session'
